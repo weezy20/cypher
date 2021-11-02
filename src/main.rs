f487949c549148ddd34e2148df79a0cb1a877599
Abhishek Shah <abhishekshah3@gmail.com>
2021-11-02 13:56:21 +0530

    restructure

--- src/main.rs
+++ src/main.rs
@@ -36,38 +36,6 @@ fn test_vigenere() {
     // println!("Decrypted names: {:?}", decrypted_names);
 }
 
-mod vigenere {
-    #![allow(unused)]
-    use super::*;
-    pub(crate) fn encrypt(data: &str, key: &str) -> String {
-        let mut keys = get_shift_vals(&key);
-        let mut res = String::with_capacity(data.len());
-        res = data
-            .chars()
-            .map(|ch| char_shift(ch, keys.next().unwrap() as i8))
-            .collect();
-        res
-    }
-    pub(crate) fn decrypt(data: &str, key: &str) -> String {
-        // We cannot accept the previous keys as the cycle may have left it in an unsound state, so
-        // we instantiate our own cycle here:
-        let mut keys = get_shift_vals(&key);
-
-        data.chars()
-            .map(|ch| char_shift(ch, -(keys.next().unwrap() as i8)))
-            .collect::<String>()
-    }
-    // Take a string key and return a cycle over shift values which are positions of individual letters
-    fn get_shift_vals(key: &str) -> impl Iterator<Item = u8> {
-        key.to_lowercase()
-            .chars()
-            .map(|ch| ch as u8 - 97)
-            .collect::<Vec<u8>>()
-            .into_iter()
-            .cycle()
-    }
-}
-
 fn test_caesar() {
     // A caesar cipher simply all letters of a message by a fixed amount
     if env::args().any(|x| x == "trace" || x == "t") {
@@ -101,6 +69,38 @@ fn test_caesar() {
         }
     }
 }
+mod vigenere {
+    #![allow(unused)]
+    use super::*;
+    pub(crate) fn encrypt(data: &str, key: &str) -> String {
+        let mut keys = get_shift_vals(&key);
+        let mut res = String::with_capacity(data.len());
+        res = data
+            .chars()
+            .map(|ch| char_shift(ch, keys.next().unwrap() as i8))
+            .collect();
+        res
+    }
+    pub(crate) fn decrypt(data: &str, key: &str) -> String {
+        // We cannot accept the previous keys as the cycle may have left it in an unsound state, so
+        // we instantiate our own cycle here:
+        let mut keys = get_shift_vals(&key);
+
+        data.chars()
+            .map(|ch| char_shift(ch, -(keys.next().unwrap() as i8)))
+            .collect::<String>()
+    }
+    // Take a string key and return a cycle over shift values which are positions of individual letters
+    fn get_shift_vals(key: &str) -> impl Iterator<Item = u8> {
+        key.to_lowercase()
+            .chars()
+            .map(|ch| ch as u8 - 97)
+            .collect::<Vec<u8>>()
+            .into_iter()
+            .cycle()
+    }
+}
+
 mod caesar {
     #![allow(unused)]
     use super::*;
